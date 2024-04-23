bcdb5a42e43a228a3e4f007169aa2101dbead010
lugroc <lucianogroc@gmail.com>
2024-04-23 18:07:14 -0400

    header and cv

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
       <div
         className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[28rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
       />
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 top-[1rem] sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="flex fixed left-1/2 h-12 -translate-x-1/2 py-2 top-[1rem] sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <li
